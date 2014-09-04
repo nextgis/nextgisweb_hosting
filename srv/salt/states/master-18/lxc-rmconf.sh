b50ae438f3f061a211f3c2596f6ed8ee3878515b
Nikita Shmyrev <the-l-a-cat@gmail.com>
2014-09-04 00:12:54 +0000

    A little more directory structure.

--- srv/salt/states/master-18/lxc-rmconf.sh
+++ srv/salt/states/master-18/lxc-rmconf.sh
@@ -8,5 +8,7 @@ initctl stop lxc-mounts lvname=${lvname} lvgroup=${lvgroup}
 rm ~virtualizer/lxc/${lvgroup}-${lvname}/config
 rmdir ~virtualizer/lxc/${lvgroup}-${lvname}/rootfs
 rmdir ~virtualizer/lxc/${lvgroup}-${lvname}
+tar -cf ~virtualizer/data.d/${lvgroup}-${lvname}.tar ~virtualizer/data.d/${lvgroup}-${lvname}
+rm -rf ~virtualizer/data.d/${lvgroup}-${lvname}
 lvremove --force ${lvgroup}/${lvname}
 
